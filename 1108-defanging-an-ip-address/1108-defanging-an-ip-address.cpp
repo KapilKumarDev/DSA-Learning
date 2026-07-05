ae32879d34d9b77456c39e3924cffd624035ca59
Kapil Kumar <kapil.2410024@gmail.com>
2026-07-05 12:58:02 +0530

    Time: 0 ms (100%), Space: 7.6 MB (75.81%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -3,8 +3,8 @@ public:
     string defangIPaddr(string address) {
         string ans;
         for(int i=0; i<address.size(); i++){
-            if(address[i]!='.') ans += address[i];
-            else ans += "[.]";
+            if(address[i]=='.') ans += "[.]";
+            else ans += address[i];
         }
         return ans;
     }
